8a0927b21b402fc84db7423c420701a36649ca54
haiduong-00 <dinhhaiduong203@gmail.com>
2021-09-18 10:21:45 +0700

    Update

--- module.go
+++ module.go
@@ -4,8 +4,12 @@ import "fmt"
 
 
 // WriteHello được dùng để in chữ Hello ra Console
+/*
+To go back to where you were, just check out the branch you were on again. (If you've made changes, as always when switching branches, you'll have to deal with them as appropriate. You could reset to throw them away; you could stash, checkout, stash pop to take them with you; you could commit them to a branch there if you want a branch there.)
+*/
 func WriteHello() {
 	fmt.Println("Hello")
+	fmt.Println("Tính năng mới: Viết doc nhiều dòng")
 }
 
 // foo là private (chỉ có trong package, không xuất ra ngoài)
