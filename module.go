e8ece058d6d8b4789c2489891b458cce94185246
haiduong-00 <dinhhaiduong203@gmail.com>
2021-09-20 08:54:11 +0700

    cho foo lên đầu

--- module.go
+++ module.go
@@ -13,9 +13,9 @@ import "fmt"
 To go back to where you were, just check out the branch you were on again. (If you've made changes, as always when switching branches, you'll have to deal with them as appropriate. You could reset to throw them away; you could stash, checkout, stash pop to take them with you; you could commit them to a branch there if you want a branch there.)
 */
 func WriteHello() {
+	foo()
 	fmt.Println("Hello")
 	fmt.Println("Tính năng mới: Viết doc nhiều dòng")
-	foo()
 }
 
 // foo là private (chỉ có trong package, không xuất ra ngoài (exported))
