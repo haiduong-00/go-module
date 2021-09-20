1933a5d5dc0e63b24a702a4d7332f94c57817637
haiduong-00 <dinhhaiduong203@gmail.com>
2021-09-20 08:49:51 +0700

    Changecode

--- v2/module.go
+++ v2/module.go
@@ -12,13 +12,20 @@ import "fmt"
 /*
 To go back to where you were, just check out the branch you were on again. (If you've made changes, as always when switching branches, you'll have to deal with them as appropriate. You could reset to throw them away; you could stash, checkout, stash pop to take them with you; you could commit them to a branch there if you want a branch there.)
 */
+//		add bar
 func WriteHello() {
 	fmt.Println("Hello")
 	fmt.Println("Tính năng mới: Viết doc nhiều dòng")
 	foo()
+	bar()
 }
 
 // foo là private (chỉ có trong package, không xuất ra ngoài (exported))
 func foo() {
 	fmt.Println("foo")
+}
+
+// bar cũng là private
+func bar()  {
+	fmt.Println("In bar")
 }
