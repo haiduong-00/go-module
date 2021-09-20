1ce0a6ba928aa70b90800be51fe549bf178d0224
haiduong-00 <dinhhaiduong203@gmail.com>
2021-09-20 08:47:36 +0700

    Update v2

---
+++ v2/go.mod
@@ -0,0 +1,3 @@
+module github.com/haiduong-00/go-module/v2
+
+go 1.16
---
+++ v2/module.go
@@ -0,0 +1,24 @@
+// Thử nghiệm publish một module lên pkg.go.dev
+//
+// Cái này sẽ xuất hiện trong v0.3.0
+//
+// Ok
+package gomodule
+
+import "fmt"
+
+
+// WriteHello được dùng để in chữ Hello ra Console
+/*
+To go back to where you were, just check out the branch you were on again. (If you've made changes, as always when switching branches, you'll have to deal with them as appropriate. You could reset to throw them away; you could stash, checkout, stash pop to take them with you; you could commit them to a branch there if you want a branch there.)
+*/
+func WriteHello() {
+	fmt.Println("Hello")
+	fmt.Println("Tính năng mới: Viết doc nhiều dòng")
+	foo()
+}
+
+// foo là private (chỉ có trong package, không xuất ra ngoài (exported))
+func foo() {
+	fmt.Println("foo")
+}
